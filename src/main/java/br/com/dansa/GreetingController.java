b16abed3ce88b6826f8088beb679d83dfeefd8cc
Jose Aderivan <jose.araujo@bancointer.com.br>
2022-01-06 00:43:00 -0300

    corrigindo o package principal

--- src/main/java/br/com/dansa/GreetingController.java
+++ src/main/java/br/com/dansa/GreetingController.java
@@ -1,4 +1,4 @@
-package br.com.dansa.restwithspringboot;
+package br.com.dansa;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
